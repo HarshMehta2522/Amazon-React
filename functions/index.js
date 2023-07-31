724fd1ec9c7747687c09d3d8792822b5dcb49cea
HarshMehta2522 <maheta.hrs2522@gmail.com>
2023-08-01 00:44:45 +0530

    bug fix

---
+++ functions/index.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51NZXGVSGyUMmynkIQK6bBxHxhTWQhvSSJkAeyutKvOqEOgAVgttQ5jdP22dUorUJR2Hqlz7anAQulWq06OQHNh3M00hy6lPhDQ"
+);
+/* eslint-inable */
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+app.get("/", (request, response) => response.status(200).send("hello"));
+app.post("/payment/create", async (request, response) => {
+  const total = request.body.amount;
+  console.log("payment", total);
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: parseInt(total, 10),
+    currency: "usd",
+  });
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+exports.api = functions.https.onRequest(app);
